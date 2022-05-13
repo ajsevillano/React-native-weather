a16f4dbb5b7c166435e9da67fb05305b08243eb5
Sevi <correosevi@gmail.com>
2022-05-13 15:29:37 +0100

    Create structure for additional info

--- App.js
+++ App.js
@@ -5,20 +5,47 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   Button,
   TextInput,
   TouchableOpacity,
 } from 'react-native';
-import { useState } from 'react';
+import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  TSpan,
+  TextPath,
+  Path,
+  Polygon,
+  Polyline,
+  Line,
+  Rect,
+  Use,
+  Symbol,
+  Defs,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+  ClipPath,
+  Pattern,
+  Mask,
+} from 'react-native-svg';
+
+import { useState, useEffect } from 'react';
 
 export default function App() {
   const [example, setExample] = useState('default');
-  const [name, setName] = useState('');
+  const [name, setName] = useState('Lewes');
   const [current, setCurrent] = useState('');
   const [weekly, setWeekly] = useState([]);
   const [fetchError, setFetchError] = useState('');
   const [cityName, setCityName] = useState(``);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const fetchData = async (city) => {
     try {
@@ -45,6 +72,13 @@ export default function App() {
     }
   };
 
+  const getTime = (timestamp) => {
+    const milliseconds = timestamp * 1000;
+    const dateObject = new Date(milliseconds);
+    const time = dateObject.toString().split(' ');
+    return time[4]?.split('').slice(0, 5);
+  };
+
   return (
     <View style={styles.container}>
       {/* <Text style={styles.header}>{fetchError && fetchError}</Text>
@@ -52,23 +86,65 @@ export default function App() {
 
     
       <StatusBar style="auto" /> */}
-      <View style={styles.inputContainer}>
-        <TextInput onChangeText={(val) => setName(val)} style={styles.input} />
-        {/* A button that calls the fetchData function. */}
-        <TouchableOpacity onPress={fetchData}>
+      <StatusBar backgroundColor="#f5f5f5" />
+      {/* <View style={styles.inputContainer}>
+        <TextInput onChangeText={(val) => setName(val)} style={styles.input} /> */}
+      {/* A button that calls the fetchData function. */}
+      {/* <TouchableOpacity onPress={fetchData}>
           <Text style={styles.buttoncolor}>Get weather</Text>
         </TouchableOpacity>
-      </View>
+      </View> */}
       {/* Conditionally render the card if there is data from the fetch. */}
       {loading && <Text>Loading...</Text>}
       {current ? (
-        <View style={styles.Card}>
-          <Text style={styles.currentWeather}>{name}</Text>
-          <Text style={styles.temperature}>{current.temp.toFixed(0)}°C</Text>
-          <Text style={styles.feelsLike}>
-            Feels like: {current.feels_like.toFixed(0)}°C
-          </Text>
-        </View>
+        <>
+          <View style={styles.Card}>
+            <Svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-cloud"
+              width="88"
+              height="88"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#a4a4a4"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <Path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <Path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7h-12" />
+            </Svg>
+            <Text>{current.weather[0].description}</Text>
+            <Text style={styles.temperature}>{current.temp.toFixed(0)}°</Text>
+            <Text style={styles.currentWeather}>{name}</Text>
+            <Text style={styles.feelsLike}>
+              Feels like: {current.feels_like.toFixed(0)}°C
+            </Text>
+          </View>
+          <View style={styles.additionalInfo}>
+            <Text style={styles.moreInfo}>Additional info</Text>
+            <View style={styles.infoBlock}>
+              <Text style={styles.humidity}>Wind: {current.humidity} m/h</Text>
+              <Text style={styles.humidity}>
+                Humidity: {current.wind_speed}%
+              </Text>
+            </View>
+            <View style={styles.infoBlock}>
+              <Text style={styles.humidity}>
+                Sunrise: {getTime(current.sunrise)}
+              </Text>
+              <Text style={styles.humidity}>
+                Sunset: {getTime(current.sunset)}
+              </Text>
+            </View>
+            <View style={styles.infoBlock}>
+              <Text style={styles.humidity}>
+                Pressure: {current.pressure} mb
+              </Text>
+              <Text style={styles.humidity}>UV: {current.uvi} </Text>
+            </View>
+          </View>
+        </>
       ) : null}
     </View>
   );
@@ -77,6 +153,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
+    backgroundColor: '#f5f5f5',
     flex: 1,
     marginTop: Constants.statusBarHeight,
     color: 'white',
@@ -90,42 +167,77 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
 
-  input: {
-    flex: 1,
-    color: '#2c3e50',
-    margin: 10,
-    padding: 8,
-    fontSize: 30,
+  weatherIcon: {
+    alignSelf: 'center',
   },
-  buttoncolor: {
-    backgroundColor: 'coral',
-    margin: 10,
-    textAlign: 'center',
-    padding: 15,
-    backgroundColor: '#3498db',
-    color: 'white',
-    fontSize: 16,
-  },
+
+  // input: {
+  //   flex: 1,
+  //   color: '#2c3e50',
+  //   margin: 10,
+  //   padding: 8,
+  //   fontSize: 30,
+  // },
+  // buttoncolor: {
+  //   margin: 10,
+  //   textAlign: 'center',
+  //   padding: 15,
+  //   backgroundColor: '#3498db',
+  //   fontSize: 16,
+  // },
   Card: {
     flexDirection: 'column',
+    alignItems: 'center',
     margin: 20,
     borderRadius: 10,
     padding: 20,
-    backgroundColor: '#3498db',
-  },
-  currentWeather: {
-    fontSize: 26,
-    color: 'white',
   },
 
   temperature: {
-    fontSize: 50,
+    fontSize: 90,
     fontWeight: 'bold',
-    color: 'white',
+    marginLeft: 10,
+    color: '#273365',
   },
+
+  currentWeather: {
+    fontSize: 26,
+    color: '#818181',
+    margin: 0,
+    padding: 0,
+  },
+
   feelsLike: {
     marginTop: 5,
-    color: 'white',
     fontSize: 16,
+    color: '#aaaaaa',
+  },
+  additionalInfo: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+
+  moreInfo: {
+    fontSize: 20,
+    marginTop: 40,
+    marginBottom: 10,
+    marginLeft: 40,
+    marginRight: 40,
+    borderRadius: 10,
+    fontWeight: 'bold',
+    color: '#273365',
+  },
+
+  humidity: {
+    fontSize: 17,
+    color: '#777575',
+  },
+
+  infoBlock: {
+    flexDirection: 'row',
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 10,
+    justifyContent: 'space-between',
   },
 });
