710b9e86b225248123420b8dc3bdce2522a7c4d7
Sevi <correosevi@gmail.com>
2022-05-27 11:44:33 +0100

    Test

--- __tests__/libs/getIcons.test.js
+++ __tests__/libs/getIcons.test.js
@@ -1,12 +1,25 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const checkFirstTimeUsingApp = async () => {
-  // Getting the firstTimeRunningApp of the key `isFirstTime` from the AsyncStorage.
-  await AsyncStorage.getItem('isFirstTime');
-};
+// const checkFirstTimeUsingApp = async () => {
+//   // Getting the firstTimeRunningApp of the key `isFirstTime` from the AsyncStorage.
+//   await AsyncStorage.getItem('isFirstTime');
+// };
 
-it('checks if Async Storage is used', async () => {
-  await checkFirstTimeUsingApp();
+// it('checks if Async Storage is used', async () => {
+//   await checkFirstTimeUsingApp();
 
-  expect(AsyncStorage.getItem).toBeCalledWith('isFirstTime');
+//   expect(AsyncStorage.getItem).toBeCalledWith('isFirstTime');
+// });
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Grid from '../../components/Grid';
+
+describe('<Grid />', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<Grid />).toJSON();
+    console.log(tree.length);
+    expect(tree).toMatchSnapshot();
+  });
 });
---
+++ components/Grid/index.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+const Grid = () => {
+  return (
+    <View>
+      <Text>Grid</Text>
+      <Text>Grid</Text>
+    </View>
+  );
+};
+
+export default Grid;
