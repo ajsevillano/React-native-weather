2012908308974ee3405211ed3ca871de734dfde2
Sevi <correosevi@gmail.com>
2025-02-25 19:59:59 +0000

    Feat | Revamp context to handle themes

--- context/theme.js
+++ context/theme.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import { useState, createContext } from 'react';
 
-const defaultValue = undefined;
-const Context = React.createContext(defaultValue);
+// Dark mode y light mode
+const themes = {
+  light: {
+    background: 'white',
+    text: '#273365',
+  },
+  dark: {
+    background: '#222222',
+    text: 'white',
+  },
+};
 
-export default Context;
+// Create context
+const ThemeContext = createContext({
+  theme: themes.light,
+  toggleTheme: () => {},
+});
+
+// Provider
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(themes.light);
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === themes.light ? themes.dark : themes.light));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
