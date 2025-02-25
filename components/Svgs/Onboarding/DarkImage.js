86df3237515420fdf6d172cabd196ebb9302d675
Sevi <correosevi@gmail.com>
2025-02-25 19:56:39 +0000

    Chore | Install typescript

--- components/Svgs/Onboarding/DarkImage.js
+++ components/Svgs/Onboarding/DarkImage.js
@@ -1,6 +1,6 @@
 import Svg, { Path, Circle } from 'react-native-svg';
 
-const DarkImage = ({ width, height, theme }) => (
+const DarkImage = ({}) => (
   <Svg
     xmlns='http://www.w3.org/2000/svg'
     width='877'
