84d3d95e74a3d016aab7f7dd1752440a30c2de46
Sevi <correosevi@gmail.com>
2025-03-01 12:58:30 +0000

    Update version

--- components/Svgs/Humidity.tsx
+++ components/Svgs/Humidity.tsx
@@ -2,20 +2,19 @@ import Svg, { Path } from 'react-native-svg';
 
 const Humidity = ({ props, theme }) => (
   <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="icon icon-tabler icon-tabler-droplet"
+    className='icon icon-tabler icon-tabler-droplet'
     width={20}
     height={20}
-    viewBox="0 0 24 24"
+    viewBox='0 0 24 24'
     strokeWidth={1.5}
     stroke={theme === 'light' ? '#273365' : '#d3d9ff'}
-    fill="none"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
     {...props}
   >
-    <Path d="M0 0h24v24H0z" stroke="none" />
-    <Path d="M6.8 11a6 6 0 1 0 10.396 0l-5.197-8-5.2 8z" />
+    <Path d='M0 0h24v24H0z' stroke='none' />
+    <Path d='M6.8 11a6 6 0 1 0 10.396 0l-5.197-8-5.2 8z' />
   </Svg>
 );
 
